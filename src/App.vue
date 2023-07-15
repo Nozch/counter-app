8fa86404c9e3848fb23a364051e6df3838cd078f
Nozch <freegia@gmail.com>
2023-07-15 15:50:21 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+import Counter from './components/Counter.vue' // あなたのCounterコンポーネントがあるパスに適宜変更してください
+
+export default {
+  components: {
+    Counter // Counterコンポーネントをローカルコンポーネントとして登録
+  }
+}
+</script>
+<template>
+  <div id="app">
+    <Counter /> <!-- ここでCounterコンポーネントを使用 -->
+  </div>
+</template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return { count: 0 }
+    },
+    methods:  {
+        increment() {
+            this.count++
+        },
+        decrement() {
+            this.count--
+        }
+    }
+}
+</script>
+<template>
+    <h1>{{ count }}</h1>
+    <button
+        @click="increment"
+    >Increase</button>
+    <button
+        @click="decrement"
+    >Decrease</button>
+</template>
